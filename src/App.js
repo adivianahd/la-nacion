91762593fdaeb142cb1dd15462abeb3d43df5ff1
adivianahd <adivianahd07@gmail.com>
2021-09-12 14:32:22 -0300

    add maquee atom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { LargeLogo } from "./components/atoms/Icons";
+import Marquee from "./components/atoms/Marquee";
 import "./theme/_theme.module.scss";
 
 function App() {
   return (
     <div>
+      <Marquee marquee="$00,00" label="$00,00" separator />
       <LargeLogo />
     </div>
   );
---
+++ src/components/atoms/Marquee/index.jsx
@@ -0,0 +1,35 @@
+import Typography from "../Typography";
+import "./styles.scss";
+
+const Marquee = ({ marquee, label, sign, separator, color }) => {
+  const withSeparator = separator ? " /" : "";
+
+  return (
+    <div>
+      {marquee && (
+        <div className="marquee-styles">
+          <Typography
+            size="XS"
+            weight="bold"
+            color={color}
+          >{`${marquee}${withSeparator}`}</Typography>
+          <Typography size="XS" weight="bold" color={color}>
+            {label}
+          </Typography>
+        </div>
+      )}
+      {sign && (
+        <div className="marquee-styles">
+          <Typography weight="bold" size="M" color={color}>
+            {sign}
+          </Typography>
+          <Typography weight="bold" size="M" color={color}>
+            {label}
+          </Typography>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Marquee;
