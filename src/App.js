bbf96c896eb1d85893e828a0e8e88c067b5d02d7
Astrid Hernandez <adiviana@MacBook-Pro-de-Jose.local>
2021-09-11 22:56:02 -0300

    WIP typography component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import GeneralButton from "./components/atoms/Button/GeneralButton";
 import { ReactComponent as MenuIcon } from "./components/atoms/Icons/search.svg";
+import Typography from "./components/atoms/Typography/Typography";
 import "./theme/theme.scss";
 
 function App() {
   return (
     <div>
+      <Typography />
       <GeneralButton Icon={MenuIcon} title="Secciones" />
     </div>
   );
---
+++ src/components/atoms/Typography/Typography.jsx
@@ -0,0 +1,11 @@
+import "./styles.scss";
+
+const Typography = () => {
+  return (
+    <div className="typography">
+      <h1>holllllaaa</h1>
+    </div>
+  );
+};
+
+export default Typography;
